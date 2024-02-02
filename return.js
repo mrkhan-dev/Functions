f754f65b2d1a7fb5351446d413428e6a1a13fcad
mrkhan-dev <sk5094625@gmail.com>
2024-02-02 22:18:16 +0600

    trying conditional return using function

--- return.js
+++ return.js
@@ -5,3 +5,13 @@ function tenTimes(number) {
 // tenTimes(20);
 const finalNumber = tenTimes(5);
 console.log(finalNumber);
+
+// conditional return
+function isOdd(inpNum) {
+  if (inpNum % 2 === 1) {
+    return true;
+  }
+  return false;
+}
+console.log(isOdd(41));
+console.log(isOdd(40));
