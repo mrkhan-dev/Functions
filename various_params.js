3763475952f1ff398e2ffb0f5660f7cc9b45699c
mrkhan-dev <sk5094625@gmail.com>
2024-02-02 23:26:48 +0600

    added somoe code

--- various_params.js
+++ various_params.js
@@ -8,5 +8,18 @@ function evenStr(str) {
   }
 }
 
-evenStr("shahjalal");
-evenStr("khan");
+evenStr("shahjalal"); //shahjalal 9 odd size
+evenStr("khan"); //khan 4 even size
+
+function doubleOrTriple(number, double) {
+  if (double) {
+    const result = number * 2;
+    return result;
+  } else {
+    const result = number * 3;
+    return result;
+  }
+}
+
+console.log(doubleOrTriple(4, false)); //12
+console.log(doubleOrTriple(40, true)); //80
